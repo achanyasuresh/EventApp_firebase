8e8a7c096d1532a2d6af531ef11db572b4ab2ae1
achanyasuresh <achanyapsuresh96@gmail.com>
2022-02-14 11:51:07 +0530

    alert removed

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,5 +1,5 @@
-import React , {useState} from 'react';
-import {useHistory } from "react-router-dom"
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom"
 import "./Styles.css";
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -14,80 +14,79 @@ import Select from '@material-ui/core/Select';
 import Link from '@material-ui/core/Link';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import {login} from "../contexts/AuthContext"
+import { login } from "../contexts/AuthContext"
 
 const Login = () => {
-  const avatarStyle = {backgroundColor: "green"}
-  const [form,setForm] = useState({
-    email:'',
-    password:''
-})
+  const avatarStyle = { backgroundColor: "green" }
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
   const history = useHistory();
 
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
     await login(form);
-    alert(Error);
-  
+    
     history.push("/dashboard")
-}
+  }
 
-  const toSignup = () =>{
+  const toSignup = () => {
     history.push("/signup")
   }
   return (
     <Grid>
       <Paper elevation={10} className="paperStyle">
         <Grid align='center'>
-          <Avatar className="avatarStyle" color="primary" style={ avatarStyle}> 
+          <Avatar className="avatarStyle" color="primary" style={avatarStyle}>
             <LockOutlinedIcon />
           </Avatar>
           <h2 className='headerStyle'>Login</h2>
           <Typography variant='caption' gutterBottom>Login With your credenials</Typography>
         </Grid>
         <Grid>
-        <form onSubmit={handleSubmit} >
-          <TextField id="email" fullWidth label="Email" placeholder='Enter your Email'  required 
-          onChange={(e) => 
-            setForm({...form, email: e.target.value})}
-          />
-          <br /> <br />
-          
-          <TextField  id='password'  fullWidth label="Password" type= "password" placeholder='Enter your password' required
-          onChange={(e) => 
-            setForm({...form, password: e.target.value})}
-          />
-          <FormControlLabel
-            control={<Checkbox
-              name="checkedA" />}
-            label="Remember Me"
-          />
-           <br /> <br />
-             <div> 
-          <Button 
-          className='buttonStyle'
-          fullWidth 
-          type='submit'
-           variant="contained"
-            color="primary" 
-            >  
-            Login
-            </Button>
+          <form onSubmit={handleSubmit} >
+            <TextField id="email" fullWidth label="Email" placeholder='Enter your Email' required
+              onChange={(e) =>
+                setForm({ ...form, email: e.target.value })}
+            />
             <br /> <br />
-            <Typography >
-            <Link href="#" >
-                Forgot Password
-              </Link>
-            </Typography>
-            <br />
-            <Typography >
-              Do you have an account ?
-            <Link href="#" onClick={toSignup}>
-                Sign Up
-              </Link>
-            </Typography>
-          </div>  
-        </form>
+
+            <TextField id='password' fullWidth label="Password" type="password" placeholder='Enter your password' required
+              onChange={(e) =>
+                setForm({ ...form, password: e.target.value })}
+            />
+            <FormControlLabel
+              control={<Checkbox
+                name="checkedA" />}
+              label="Remember Me"
+            />
+            <br /> <br />
+            <div>
+              <Button
+                className='buttonStyle'
+                fullWidth
+                type='submit'
+                variant="contained"
+                color="primary"
+              >
+                Login
+              </Button>
+              <br /> <br />
+              <Typography >
+                <Link href="#" >
+                  Forgot Password
+                </Link>
+              </Typography>
+              <br />
+              <Typography >
+                Do you have an account ?
+                <Link href="#" onClick={toSignup}>
+                  Sign Up
+                </Link>
+              </Typography>
+            </div>
+          </form>
         </Grid>
 
       </Paper>
